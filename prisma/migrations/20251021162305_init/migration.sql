9e82cc0ccf3110f06614fe4fe5e4600816dc6350
bigkid-dev <egbeyemifisola777@gmail.com>
2025-10-22 19:11:40 +0100

    fix(prisma): fixed the prisma seeds error

--- prisma/migrations/20251021162305_init/migration.sql
+++ prisma/migrations/20251021162305_init/migration.sql
@@ -19,15 +19,16 @@ CREATE TABLE "Student" (
     "name" TEXT NOT NULL,
     "surname" TEXT NOT NULL,
     "email" TEXT,
-    "phone" TEXT NOT NULL,
+    "phone" TEXT,
     "address" TEXT NOT NULL,
-    "img" TEXT NOT NULL,
+    "img" TEXT,
     "bloodType" TEXT NOT NULL,
     "sex" "UserSex" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "parentId" INTEGER NOT NULL,
+    "parentId" TEXT NOT NULL,
     "classId" INTEGER NOT NULL,
     "gradeId" INTEGER NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
 );
@@ -39,20 +40,20 @@ CREATE TABLE "Teacher" (
     "name" TEXT NOT NULL,
     "surname" TEXT NOT NULL,
     "email" TEXT,
-    "phone" TEXT NOT NULL,
+    "phone" TEXT,
     "address" TEXT NOT NULL,
-    "img" TEXT NOT NULL,
+    "img" TEXT,
     "bloodType" TEXT NOT NULL,
     "sex" "UserSex" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "subjectId" TEXT NOT NULL,
+    "birthday" TIMESTAMP(3),
 
     CONSTRAINT "Teacher_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Parent" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "surname" TEXT NOT NULL,
@@ -66,7 +67,7 @@ CREATE TABLE "Parent" (
 
 -- CreateTable
 CREATE TABLE "Grade" (
-    "id" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
     "level" INTEGER NOT NULL,
 
     CONSTRAINT "Grade_pkey" PRIMARY KEY ("id")
@@ -77,7 +78,7 @@ CREATE TABLE "Class" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "capacity" INTEGER NOT NULL,
-    "supervisorsId" TEXT NOT NULL,
+    "supervisorId" TEXT,
     "gradeId" INTEGER NOT NULL,
 
     CONSTRAINT "Class_pkey" PRIMARY KEY ("id")
@@ -86,6 +87,7 @@ CREATE TABLE "Class" (
 -- CreateTable
 CREATE TABLE "Subject" (
     "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
 
     CONSTRAINT "Subject_pkey" PRIMARY KEY ("id")
 );
@@ -93,6 +95,7 @@ CREATE TABLE "Subject" (
 -- CreateTable
 CREATE TABLE "Lesson" (
     "id" SERIAL NOT NULL,
+    "name" TEXT,
     "day" "Day" NOT NULL,
     "startTime" TIMESTAMP(3) NOT NULL,
     "endTime" TIMESTAMP(3) NOT NULL,
@@ -117,6 +120,7 @@ CREATE TABLE "Exam" (
 -- CreateTable
 CREATE TABLE "Assignment" (
     "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
     "startDate" TIMESTAMP(3) NOT NULL,
     "dueDate" TIMESTAMP(3) NOT NULL,
     "lessonId" INTEGER NOT NULL,
@@ -151,9 +155,9 @@ CREATE TABLE "Event" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
     "classId" INTEGER,
-    "startDate" TIMESTAMP(3) NOT NULL,
-    "endDate" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
 );
@@ -163,8 +167,8 @@ CREATE TABLE "Announcement" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "classId" INTEGER,
     "date" TIMESTAMP(3) NOT NULL,
+    "classId" INTEGER,
 
     CONSTRAINT "Announcement_pkey" PRIMARY KEY ("id")
 );
@@ -214,6 +218,9 @@ CREATE UNIQUE INDEX "Grade_level_key" ON "Grade"("level");
 CREATE UNIQUE INDEX "Class_name_key" ON "Class"("name");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Subject_name_key" ON "Subject"("name");
+
+-- CreateIndex
 CREATE INDEX "_SubjectToTeacher_B_index" ON "_SubjectToTeacher"("B");
 
 -- AddForeignKey
@@ -226,7 +233,7 @@ ALTER TABLE "Student" ADD CONSTRAINT "Student_classId_fkey" FOREIGN KEY ("classI
 ALTER TABLE "Student" ADD CONSTRAINT "Student_gradeId_fkey" FOREIGN KEY ("gradeId") REFERENCES "Grade"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Class" ADD CONSTRAINT "Class_supervisorsId_fkey" FOREIGN KEY ("supervisorsId") REFERENCES "Teacher"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Class" ADD CONSTRAINT "Class_supervisorId_fkey" FOREIGN KEY ("supervisorId") REFERENCES "Teacher"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Class" ADD CONSTRAINT "Class_gradeId_fkey" FOREIGN KEY ("gradeId") REFERENCES "Grade"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "../../generated/prisma"
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient, Prisma } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { 
     prisma: PrismaClient
